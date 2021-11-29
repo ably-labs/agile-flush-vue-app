d798ef2c2b90154c8ffeecb801d34e97e13eceb4
Marc Duiker <marcduiker@gmail.com>
2021-11-29 13:15:43 +0100

    Update layout with session

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,57 @@
 <template>
   <div>
     <h1>Planning Poker</h1>
-    <p>Invite your team members by copying the url and start voting 🚀!</p>
-    <p>
-      Once everyone has submitted their vote, click the button to show the
-      results.
-    </p>
-    <button @click="toggleResults">
-      {{ showResults ? "Hide votes" : "Show votes" }}
+    <ol>
+      <li>Start a new session.</li>
+      <li>Invite your team members by copying &amp; sharing the url.</li>
+      <li>Start voting 🚀!</li>
+    </ol>
+    <button @click="startSession">
+      {{ sessionStarted ? "Start new session" : "Start session" }}
     </button>
-    <h2>Cards</h2>
-    <p>Click on a card to vote. To undo your vote, click the card again.</p>
-    <div class="cardlist">
-      <div
-        class="card"
-        v-for="(card, index) in cards"
-        :key="card.number"
-        :id="card.number"
-        @click="vote(index, card.number)"
-      >
-        <p :class="{ selected: selected === card.number }">{{ card.visual }}</p>
-        <p class="votecount" v-if="showResults">{{ card.count }}</p>
+    <div v-if="sessionStarted">
+      <h2>Session: {{ inviteLink }}</h2>
+      <p>
+        Once everyone has submitted their vote, click the button to show the
+        results.
+      </p>
+      <button @click="toggleResults">
+        {{ showResults ? "Hide votes" : "Show votes" }}
+      </button>
+      <h3>Cards</h3>
+      <p>Click on a card to vote. To undo your vote, click the card again.</p>
+      <div class="cardlist">
+        <div
+          class="card"
+          v-for="(card, index) in cards"
+          :key="card.number"
+          :id="card.number"
+          @click="vote(index, card.number)"
+        >
+          <p :class="{ selected: selected === card.number }">
+            {{ card.visual }}
+          </p>
+          <p class="votecount" v-if="showResults">{{ card.count }}</p>
+        </div>
       </div>
     </div>
-    <p>Powered by <a href="https://www.ably.com" target="_blank">ably.com</a>.</p>
+    <p>
+      Powered by <a href="https://www.ably.com" target="_blank">ably.com</a>.
+    </p>
   </div>
 </template>
 
 <script>
+import { generateName } from "./util/nameGenerator.js";
+import { LinkGenerator } from "./util/linkGenerator.js";
+const urlParams = new URLSearchParams(location.search);
+const querySessionId = urlParams.get("sessionId");
 export default {
   name: "App",
   data() {
     return {
+      sessionStarted: false,
+      sessionId: querySessionId || generateName(),
       selected: null,
       showResults: false,
       cards: [
@@ -162,6 +182,10 @@ export default {
     toggleResults() {
       this.showResults = !this.showResults;
     },
+    startSession() {
+      this.sessionStarted = true;
+      this.$router.push({ path: '', query: { sessionId: this.sessionId } })
+    },
     vote(cardIndex, number) {
       if (this.selected === null) {
         this.selected = number;
@@ -172,6 +196,12 @@ export default {
       }
     },
   },
+  computed: {
+    inviteLink: function () {
+      const linkGenerator = new LinkGenerator(window.location);
+      return linkGenerator.linkTo({ sessionId: this.sessionId });
+    }
+  }
 };
 </script>
 
@@ -207,7 +237,7 @@ body {
 }
 
 a {
-  color: #e40000
+  color: #e40000;
 }
 
 button {
