f2ee504942197ae2ecbcb9a0efa9b79f3be56e00
Marc Duiker <marcduiker@gmail.com>
2021-12-06 15:36:34 +0100

    Fix client ID generation

--- api/createTokenRequest/index.js
+++ api/createTokenRequest/index.js
@@ -1,8 +1,9 @@
 const Ably = require('ably/promises');
 
-module.exports = async function (context, req) {
+module.exports = async function (context) {
+    let id = 'id-' + Math.random().toString(36).substr(2, 16);
     const client = new Ably.Realtime(process.env.ABLY_API_KEY);
-    const tokenRequestData = await client.auth.createTokenRequest({ clientId: 'ably-azure-static-site-demo' });    
+    const tokenRequestData = await client.auth.createTokenRequest({ clientId: id });
     context.res = { 
         headers: { "content-type": "application/json" },
         body: JSON.stringify(tokenRequestData) 
