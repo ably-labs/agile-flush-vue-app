7478fc3bdce7c0c89db6b1c8c6bc825f23167d0d
Marc Duiker <marcduiker@users.noreply.github.com>
2021-12-13 12:20:36 +0100

    Add emoji

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <p>Made with <span class="red">❤</span> by <a href="https://twitter.com/marcduiker" target="_blank">@marcduiker</a>. Powered by <a href="https://www.ably.com" target="_blank">ably.com</a>.</p>
+  <p>Made with <span class="red">❤</span> by <a href="https://twitter.com/marcduiker" target="_blank">@marcduiker</a>. Powered by <a href="https://www.ably.com" target="_blank">ably.com</a> 🚀.</p>
 </template>
 
 <script>
