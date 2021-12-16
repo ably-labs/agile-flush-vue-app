f4c610f9d57b653201543e66f5b9687ce9206604
Marc Duiker <marcduiker@users.noreply.github.com>
2021-12-16 15:20:27 +0100

    Update formatting

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,9 +1,13 @@
 <template>
-  <p>Made with <span class="red">❤</span> by <a href="https://twitter.com/marcduiker" target="_blank">@marcduiker</a>. Powered by <a href="https://www.ably.com" target="_blank">ably.com</a> 🚀.</p>
+  <p>
+    Made with <span class="red">❤</span> by
+    <a href="https://twitter.com/marcduiker" target="_blank">@marcduiker</a>.
+    Powered by <a href="https://www.ably.com" target="_blank">ably.com</a> 🚀.
+  </p>
 </template>
 
 <script>
-  export default {
-  name: "TheFooter"
-}
+export default {
+  name: "TheFooter",
+};
 </script>
